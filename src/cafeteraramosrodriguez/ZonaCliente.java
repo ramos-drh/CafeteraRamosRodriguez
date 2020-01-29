93197d02975e3d3f150ab8c4e7c5e8cccd88826b
David Ramos <ramos_drh@hotmail.com>
2020-01-29 20:39:34 +0100

    Creación de la clase ZonaCliente y el atributo saldoCliente + getter y setter

---
+++ src/cafeteraramosrodriguez/ZonaCliente.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cafeteraramosrodriguez;
+
+/**
+ *
+ * @author ramos
+ */
+public class ZonaCliente {
+
+    private int saldoCliente;
+    
+    public ZonaCliente() {
+        saldoCliente = 0;
+    }
+
+    public int getSaldoCliente() {
+        return saldoCliente;
+    }
+
+    public void setSaldoCliente(int saldoCliente) {
+        this.saldoCliente = saldoCliente;
+    }
+    
+    
+}
